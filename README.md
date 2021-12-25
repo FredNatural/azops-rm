83228084d9e1bccc09ac67d6fee5edbab4ea3d4b
Jim Pauley <jhpauley@protonmail.com>
2021-12-25 16:36:39 -0500

    Code to test on USACE GFE.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # azops-rm
 Testing distributed co-dev between MULE and GFE
+
+Step 0: Getting the account stuff worked out for both 'enclaves'
--- set-01.ps1
+++ set-01.ps1
@@ -3,15 +3,37 @@
 # Some PSHell and MSGraph stuff
 #--------------------------------------------------------------------
 
-$_AZMGI.ID		= "d966aebf-5d1d-4444-bb28-cc4f3f524b75"
-$_AZSUB.ID		= "96d27511-3a01-45b4-a6fe-b69d01ae62ad"
-$_AZSUB.NAME	= "curvic"
-$_AADTENANT.ID	= "d966aebf-5d1d-4444-bb28-cc4f3f524b75" # The Tenant ID is synonomous with Organization ID
-$_AADTENANT.TYPE= "AAD"
+# Connect as jh.pauley@outlook.com into curvic.io
 
+$_Environment		 = "AzureCloud"
+$_TenantType		 = "AAD"
+$_TenantId			 = "d966aebf-5d1d-4444-bb28-cc4f3f524b75" # The Tenant ID is synonomous with Organization ID is synonomous with Management Group ID
+$_SubscriptionId	 = "96d27511-3a01-45b4-a6fe-b69d01ae62ad"
+$_SubscriptionName	 = "curvic.io.s"
+
+<# Connect as james.h.pauley2 into USACE Azure IL45 Cloud
+
+$_Environment		 = "AzureUSGovernment"
+$_TenantType		 = "AAD"
+$_TenantId			 = "fc4d76ba-f17c-4c50-b9a7-8f3163d27582" # The Tenant ID is synonomous with Organization ID is synonomous with Management Group ID
+$_SubscriptionId	 = "2e904e96-8207-4b76-bd36-d1e58766bec5"
+$_SubscriptionName	 = "Azure Government Enterprise"
+
+#>
+
+# First Breakfast
+Connect-AzAccount -EnvironmentName AzureCloud -TenantId $_TenantId
+Connect-AzureAD -AzureEnvironmentName AzureCloud -TenantId $_TenantId
+Set-AzContext -Subscription $_SubscriptionId -Tenant $_TenantId
+
+# Trust but verify
+Get-AzContext
 
 Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
 Get-MgUser
-# It works
+
+# There are currently 2 versions of Microsoft Graph REST APIs - v1.0 and beta, we're being brave..
 Select-MgProfile -Name "beta"
+
+# Clean the kitchen
 Disconnect-MgGraph
