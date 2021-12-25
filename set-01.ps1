e17780986aea8a8da0194426bc39d2f9b13eedf3
james.h.pauley2 <james.h.pauley2@usace.army.mil>
2021-12-25 18:00:39 -0500

    minor changes to support USACE tenant

--- set-01.ps1
+++ set-01.ps1
@@ -5,25 +5,26 @@
 
 # Connect as jh.pauley@outlook.com into curvic.io
 
-$_Environment		 = "AzureCloud"
-$_TenantType		 = "AAD"
-$_TenantId			 = "d966aebf-5d1d-4444-bb28-cc4f3f524b75" # The Tenant ID is synonomous with Organization ID is synonomous with Management Group ID
-$_SubscriptionId	 = "96d27511-3a01-45b4-a6fe-b69d01ae62ad"
-$_SubscriptionName	 = "curvic.io.s"
-
-<# Connect as james.h.pauley2 into USACE Azure IL45 Cloud
-
-$_Environment		 = "AzureUSGovernment"
-$_TenantType		 = "AAD"
-$_TenantId			 = "fc4d76ba-f17c-4c50-b9a7-8f3163d27582" # The Tenant ID is synonomous with Organization ID is synonomous with Management Group ID
-$_SubscriptionId	 = "2e904e96-8207-4b76-bd36-d1e58766bec5"
-$_SubscriptionName	 = "Azure Government Enterprise"
-
+<#$_Environment		= "AzureCloud"
+$_TenantType		= "AAD"
+$_TenantId			= "d966aebf-5d1d-4444-bb28-cc4f3f524b75" # The Tenant ID is synonymous with Organization ID is synonymous with Management Group ID
+$_SubscriptionId	= "96d27511-3a01-45b4-a6fe-b69d01ae62ad"
+$_SubscriptionName	= "curvic.io.s"
 #>
 
+# Connect as james.h.pauley2 into USACE Azure IL45 Cloud
+
+$_Environment		= "AzureUSGovernment"
+$_TenantType		= "AAD"
+$_TenantId			= "fc4d76ba-f17c-4c50-b9a7-8f3163d27582" # The Tenant ID is synonymous with Organization ID is synonymous with Management Group ID
+$_SubscriptionId	= "2e904e96-8207-4b76-bd36-d1e58766bec5"
+$_SubscriptionName	= "Azure Government Enterprise"
+
+#
+
 # First Breakfast
-Connect-AzAccount -EnvironmentName AzureCloud -TenantId $_TenantId
-Connect-AzureAD -AzureEnvironmentName AzureCloud -TenantId $_TenantId
+Connect-AzAccount -EnvironmentName $_Environment -TenantId $_TenantId
+Connect-AzureAD -AzureEnvironmentName $_Environment -TenantId $_TenantId
 Set-AzContext -Subscription $_SubscriptionId -Tenant $_TenantId
 
 # Trust but verify
